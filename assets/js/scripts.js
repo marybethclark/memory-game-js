b24b1e8f899577c241fdcd713225b1734b42fcc0
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-04 13:03:35 -0400

    add card matched function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,58 @@
+// -------------  event timer -----------------
 var start = new Date;
 
 setInterval(function() {
     $('.timer').text((new Date - start) / 1000 + " Seconds");
 }, 1000);
 
+// --------------- card flip --------------------
+
+var clickCounter = 0;
 $(".cardunflipped").on("click",function(){
+  clickCounter++
+   if (clickCounter === 1){
   $(this).addClass("cardflipped");
-});
-// toggle was making it flip back
+}
+  else if(clickCounter === 2 && $(this).attr("value")===$(".cardflipped").attr("value")){
+  $(this).addClass("cardflipped");
+  $(this).addClass("match");
+  $(".cardflipped").addClass("match");
+  $(".match").removeClass("cardflipped");
+  clickCounter=0;
+}
+// ---need to add  .setTimeout or .setInterval here, so there will be a pause before un-matched cards turn back over -------
+else if (clickCounter===2 && $(this).attr("value")!==$(".cardflipped").attr("value")){
+  $(this).addClass("cardflipped");
+  $(".cardflipped").addClass("cardunflipped");
+
+
+  $(".cardunflipped").removeClass("cardflipped");
+  clickCounter=0;
+
+}
+})
+
+
+// (function () {
+//
+//
+//   $(".cardunflipped").click(function () {
+//
+// // add 1 to the click counter
+//    if (count == 2) {
+// // when click counter reaches 2 clicks, check for a pair match (HOW???)
+//
+//     var oneOfTwo = $(val)
+//     var twoOfTwo = $(val)
+//
+//
+//      checkForMatch(function() {
+//        if (oneoftwo === twooftwo).do nothing;
+//
+//
+//
+// });
+
 
 
 // add a counter to detect 2 clicks here
